e166bb4f159a55ba872841d5f136a92eeb80ea32
Newman-cletus <57435941+Newman-cletus@users.noreply.github.com>
2025-11-08 13:15:37 +0100

    updatedPassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import ForgotPassword from "./pages/Auth/ForgotPassword";
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
