e5a47c84fee7115db11ab2a59f0c188e49c1e50f
Developergatsby <gabokekeemmanuel@gmail.com>
2025-11-13 20:23:19 +0100

    Login and Register Working properly

--- src/components/Auth/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import { useAuth } from './AuthContext'
 
-export const ProtectedRoute = ({children}) => {
-    const {isAuthenticated} = useAuth();
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
-    if(!isAuthenticated) {
-       return <Navigate to="/login" replace />
-    }
+export const ProtectedRoute = ({ children }) => {
+  // Implemented the states being isauth and loading
+  const { isAuthenticated, loading } = useAuth();
 
+  // Wait until we know whether the user is authenticated
+  if (loading) {
+    return <div>Loading...</div>; 
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
 
   return children;
-}
+};
